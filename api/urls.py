6eecde14a254772bbb0219ed048ed016f8f86b1f
Leeson <3098217897@xiaomi.com>
2025-12-04 14:19:59 +0800

    t2i接口实现

--- api/urls.py
+++ api/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
-from .views import receive_content
+from .views import receive_content, generate_image
 
 urlpatterns = [
     path('story/generate', receive_content),
+    path('shot/generate', generate_image),
 ]
